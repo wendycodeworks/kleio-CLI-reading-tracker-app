c3f5b8ac0498e6ac04be71f83a10e10827f012a3
wenzable <wendyuworks@gmail.com>
2020-03-10 10:51:28 +1100

    add ruby file - generated Book class

---
+++ terminal_app_reading_journal.rb
@@ -0,0 +1,12 @@
+# Terminal-app Reading Journal
+
+class Book
+    attr_accessor :title, :author, :publish_year, :rating
+    initialize(title, author, publish_year, rating)
+    @title = title
+    @author = author
+    @publish_year = publish_year
+    @rating = rating
+end
+
+
