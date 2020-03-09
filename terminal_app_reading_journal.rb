7e76c5b45dc676e4dd9b64147c3a17f1c773edb6
wenzable <wendyuworks@gmail.com>
2020-03-10 10:54:40 +1100

    methods added

--- terminal_app_reading_journal.rb
+++ terminal_app_reading_journal.rb
@@ -9,4 +9,23 @@ class Book
     @rating = rating
 end
 
+def add_book(title)
+
+end
+
+def delete_book(title)
+
+end
+
+def check_library()
+
+end
+
+def collect_quote()
+
+end
+
+def quote_generator()
+
+end
 
