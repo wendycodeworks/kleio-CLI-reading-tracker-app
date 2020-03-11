f0b7133495d089261894f8933b140c29f6cef4c1
wenzable <wendyuworks@gmail.com>
2020-03-11 14:18:27 +1100

    Added search/edit method, error handling and more

--- TA_CLIO_Reading_Journal.rb
+++ TA_CLIO_Reading_Journal.rb
@@ -1,29 +1,22 @@
 # TA_CLIO_Reading_Journal
 
-
 require_relative 'reading_journal_classes_methods.rb'
 require 'tty-prompt'
 
 initialise_app = true
+start_greeting
+name = "Reader"
+puts "Hi #{name}, I'm CLIO - your reading companion. I help you stay on track with your reading goals." 
 
 while initialise_app == true
 
 # puts "Hello there! What's your name?"
 # name = gets.chomp 
 
-name = "Reader"
-puts "Hi #{name}, I'm CLIO - your reading companion. I help you stay on track with your reading goals." 
-
 prompt = TTY::Prompt.new
-question = "What would you like to do?"
+question = "Select an action:"
 choices = [{"Add book" => 1}, {"Check log" => 2}, {"Manage log" => 3}, {"Exit" => 4}]
 user_input = prompt.select(question, choices) 
-# do |menu|
-    # menu.choice "Check Log"
-    # menu.choice "View progress"
-    # menu.choice "Add/Edit/Delete Book"
-    # menu.choice "Exit"
-# end
 
 main_menu_action(user_input)
 
@@ -32,12 +25,4 @@ main_menu_action(user_input)
     else 
     end
 end
-
-
-
-
-
-# pickles = add_book()
-# system("clear")
-# p pickles
 
--- reading_journal_classes_methods.rb
+++ reading_journal_classes_methods.rb
@@ -2,79 +2,146 @@
 
 #Gems
 require 'tty-box'
+require 'tty-prompt'
+require 'pastel'
+require 'tty-font'
+require 'terminal-table'
 
-@current_reads = ["Invisible Women", "Cat in the Hat by Dr Seuss", "Alice in Wonderland"]
+# User input variables
+@user_log = [@current_reads, @future_reads, @past_reads]
+@current_reads = [["Cat in the Hat", "Dr Seuss"], ["Alice in Wonderland", "Lewis Carol"]]
 @future_reads  = []
 @past_reads    = []
+@goals = {}
+@progress = {}
 
+# Book class 
 class Book
     attr_accessor :title, :author
-
     def initialize(title, author)
     @title = title
     @author = author
     puts "Successfully added #{@title} by #{@author}."
     end
+end
+
+# Goals Method
+## Setting reading goal
+def set_goals
+    # Prompt user to set a goal 
+
+    # Choose a deadline (total a month/ total in year)
+
+    # Set a number of books
+
+    # Store goal details in user info variable
 
 end
 
+## View progress based on current reading habits
+def view_progress
+    # Do in progress
 
+    # check past reads
+
+    # compare to current goals
+
+    # if complete all current reads return progress
+
+    # if complete all future reads return progress
+
+end
+
+# Book methods
+## Creating a record of a book to be stored in one of 3 lists
 def add_book()
     puts "Please enter book title:"
     @title = gets.chomp
     puts "Please enter author:"
     @author = gets.chomp
-    puts "Please choose list: \n 1. Current reads \n 2. Future reads \n 3. Past reads"
-    list_choice = gets.chomp
+
+    prompt = TTY::Prompt.new
+    question = "Please choose a log:"
+    choices = [{"Current reads" => 1}, {"Future reads" => 2}, {"Past reads" => 3}]
+    list_choice = prompt.select(question, choices)
 
     title = Book.new(@title, @author)
 
-    if list_choice == "1"
-         @current_reads << "#{@title} by #{@author}"
-    elsif list_choice == "2"
-         @current_reads << title
-    elsif list_choice == "3"
-         @current_reads << title
+    if list_choice == 1
+         @current_reads << [@title, @author]
+         return_to_mm
+    elsif list_choice == 2
+         @future_reads << [@title, @author]
+         return_to_mm
+    elsif list_choice == 3
+         @past_reads << [@title, @author]
+         return_to_mm
     end
-
     return title
-
 end
 
-def edit_book()
 #change title, author or progess
-
+def edit_book
+    # select array
+        list_choice = log_edit_selection('Please select a log')
+        #identify entry in chosen array
+        log = list_choice
+        #to separate
+        puts "Search by title or author:"
+        search_result = gets.chomp.to_s
+        
+        exist = false
+    
+        for i in 0..log.length-1
+            if log[i][0].include?(search_result)
+                p log[i][0]
+                p "omg"
+                puts "Record located. Enter new title:"
+                user_input = gets.chomp.to_s 
+                log[i][0] = user_input
+                puts "Record updated to #{log[i][0]} by #{log[i][1]}"
+                exist = true
+            elsif log[i][1].include?(search_result)
+                p log[i][1]
+                puts "Record located. Enter new author:"
+                user_input = gets.chomp.to_s 
+                log[i][1] = user_input
+                puts "Record updated to #{log[i][0]} by #{log[i][1]}"
+                exist = true
+            end
+        end
+    if exist == false
+        p 'invalid input'
+     end
 end
+    
 
-def book_dets(title)
-    #each function
-    if @current_reads.include?(title)
-        puts title
-    end
-end
-
+#remove from list
 def delete_book(title)
 
 end
 
+# User reading log functions 
+## Check/view logs
 def check_log()
-    prompt = TTY::Prompt.new
-    question = "Which log would you like to view?"
-    choices = [{"Current reads" => 1}, {"Future reads" => 2}, {"Past reads" => 3}]
-    user_input = prompt.select(question, choices)
+    system("clear")
     
-    if user_input == 1
+   list_choice = log_selection("Please select a log:")
+    
+    if list_choice == 1
         total_current = @current_reads.count()
         if total_current > 0
-            puts @current_reads
+            table = Terminal::Table.new :title => "Current Reads", :headings => ['Title', 'Author'], :rows => @current_reads
+            table.style = {:all_separators => true}
+            puts table
             box = TTY::Box.info("Total current reads: #{total_current}")
             print box
+            return_to_mm
         else
             error_in_log
-            system("clear")
             return_to_mm
         end
-    elsif user_input == 2
+    elsif list_choice == 2
         total_future = @future_reads.count()
         if total_future > 0
             puts @future_reads
@@ -84,10 +151,11 @@ def check_log()
             error_in_log
             return_to_mm()
         end
-    else user_input == 3 
+    else list_choice == 3 
         total_past = @past_reads.count()
         if total_past > 0
-            puts @past_reads
+            table = Terminal::Table.new :past_reads => @past_reads
+            puts table
             box = TTY::Box.info("Total past reads: #{total_past}")
             print box
         else
@@ -116,6 +184,7 @@ def main_menu_action(user_input)
     # manage log
         elsif user_input == 3
         puts "Coming soon"
+        return_to_mm
     # exit app
         elsif user_input ==  4
         puts "You can check out any time you like but you can never leave. *Guitar riffs*"
@@ -134,3 +203,57 @@ def error_in_log
     error = TTY::Box.warn("Sorry log is empty. Return to main menu to add a book to this log.")
     puts error
 end
+
+#   Info messages to user
+
+def start_greeting
+    font = TTY::Font.new(:doom)
+    pastel = Pastel.new
+    puts pastel.white(font.write("Hello!"))
+end
+
+#   Navigation Methods
+
+def log_selection(question)
+    prompt = TTY::Prompt.new
+    choices = [{"Current reads" => 1}, {"Future reads" => 2}, {"Past reads" => 3}]
+    list_choice = prompt.select(question, choices)
+    
+    if list_choice == 1
+        return list_choice
+    elsif list_choice == 2
+        return list_choice
+    else list_choice == 3
+        return list_choice
+    end
+    return list_choice
+end
+
+def log_edit_selection(question)
+    prompt = TTY::Prompt.new
+    choices = [{"Current reads" => 1}, {"Future reads" => 2}, {"Past reads" => 3}]
+    list_choice = prompt.select(question, choices)
+
+    if list_choice == 1
+        list_choice = @current_reads
+        return list_choice
+    elsif list_choice == 2
+        list_choice = @future_reads
+        return list_choice
+    else list_choice == 3
+        list_choice = @past_reads
+        return list_choice
+    end
+    return list_choice
+end
+
+# p @current_reads[0][0] = "Green eggs and ham"
+# p @current_reads
+
+
+# p log_selection("Please choose a log to view:")
+
+# prompt = TTY::Prompt.new
+# prompt.yes?("Does Rick like pineapples with pizza?")
+
+# puts prompt
